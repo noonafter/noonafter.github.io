dd13b6269486e734f6a179cf3d168cf5ab7eec40
noonafter <lichuan0987@gmail.com>
2026-07-21 22:20:55 +0800

    添加行搜索功能

--- _includes/search-providers/default/search.js
+++ _includes/search-providers/default/search.js
@@ -1,8 +1,12 @@
 var SOURCES = window.TEXT_VARIABLES.sources;
 var PAHTS = window.TEXT_VARIABLES.paths;
-window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
+window.Lazyload.js(SOURCES.jquery, function() {
   var search = (window.search || (window.search = {}));
   var searchData = window.TEXT_SEARCH_DATA || {};
+  var searchDataLoaded = !!window.TEXT_SEARCH_DATA;
+  var searchDataLoading = false;
+  var MAX_RESULTS_PER_COLLECTION = 20;
+  var MAX_MATCHES_PER_ARTICLE = 3;
 
   function memorize(f) {
     var cache = {};
@@ -15,33 +19,99 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
 
   /// search
   function searchByQuery(query) {
-    var i, j, key, keys, cur, _title, result = {};
+    var i, j, key, keys, cur, title, content, lines, lineIndex;
+    var normalizedQuery = query.trim().toLocaleLowerCase();
+    var result = {};
+    if (!normalizedQuery) { return result; }
+
     keys = Object.keys(searchData);
     for (i = 0; i < keys.length; i++) {
       key = keys[i];
       for (j = 0; j < searchData[key].length; j++) {
-        cur = searchData[key][j], _title = cur.title;
-        if ((result[key] === undefined || result[key] && result[key].length < 4 )
-          && _title.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
-          if (result[key] === undefined) {
-            result[key] = [];
+        if (result[key] && result[key].length >= MAX_RESULTS_PER_COLLECTION) {
+          break;
+        }
+
+        cur = searchData[key][j];
+        title = cur.title || '';
+        content = cur.content || '';
+        lines = content.split(/\r?\n/);
+
+        if (title.toLocaleLowerCase().indexOf(normalizedQuery) >= 0) {
+          addResult(result, key, cur, null, null);
+        }
+
+        var articleMatches = 0;
+        for (lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+          if (result[key] && result[key].length >= MAX_RESULTS_PER_COLLECTION
+              || articleMatches >= MAX_MATCHES_PER_ARTICLE) {
+            break;
           }
-          result[key].push(cur);
+          var line = lines[lineIndex].trim();
+          if (isSearchableLine(line)
+              && line.toLocaleLowerCase().indexOf(normalizedQuery) >= 0) {
+            addResult(result, key, cur, line, lineIndex + 1);
+            articleMatches++;
+          }
         }
       }
     }
     return result;
   }
 
+  function addResult(result, key, article, line, lineNumber) {
+    if (result[key] === undefined) {
+      result[key] = [];
+    }
+    result[key].push({
+      title: article.title,
+      url: article.url,
+      line: line,
+      lineNumber: lineNumber
+    });
+  }
+
+  function isSearchableLine(line) {
+    return line && !/^!\[[^\]]*\]\([^)]*\)\s*$/.test(line);
+  }
+
   var renderHeader = memorize(function(header) {
-    return $('<p class="search-result__header">' + header + '</p>');
+    return $('<p class="search-result__header"></p>').text(header);
   });
 
-  var renderItem = function(index, title, url) {
-    return $('<li class="search-result__item" data-index="' + index + '"><a class="button" href="' + url + '">' + title + '</a></li>');
+  function appendHighlightedText($element, value, query) {
+    var normalizedValue = value.toLocaleLowerCase();
+    var normalizedQuery = query.toLocaleLowerCase();
+    var cursor = 0;
+    var matchIndex = normalizedValue.indexOf(normalizedQuery);
+    while (matchIndex >= 0) {
+      $element.append(document.createTextNode(value.slice(cursor, matchIndex)));
+      $('<mark class="search-result__mark"></mark>')
+        .text(value.slice(matchIndex, matchIndex + query.length))
+        .appendTo($element);
+      cursor = matchIndex + query.length;
+      matchIndex = normalizedValue.indexOf(normalizedQuery, cursor);
+    }
+    $element.append(document.createTextNode(value.slice(cursor)));
+  }
+
+  var renderItem = function(index, item, query) {
+    var $item = $('<li class="search-result__item"></li>').attr('data-index', index);
+    var $link = $('<a class="button"></a>').attr('href', item.url).appendTo($item);
+    var $title = $('<span class="search-result__title"></span>').appendTo($link);
+    appendHighlightedText($title, item.title, query);
+    if (item.line) {
+      var $line = $('<span class="search-result__line"></span>').appendTo($link);
+      $('<span class="search-result__line-number"></span>')
+        .text('L' + item.lineNumber)
+        .appendTo($line);
+      var $snippet = $('<span class="search-result__snippet"></span>').appendTo($line);
+      appendHighlightedText($snippet, item.line, query);
+    }
+    return $item;
   };
 
-  function render(data) {
+  function render(data, query) {
     if (!data) { return null; }
     var $root = $('<ul></ul>'), i, j, key, keys, cur, itemIndex = 0;
     keys = Object.keys(data);
@@ -50,7 +120,7 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
       $root.append(renderHeader(key));
       for (j = 0; j < data[key].length; j++) {
         cur = data[key][j];
-        $root.append(renderItem(itemIndex++, cur.title, cur.url));
+        $root.append(renderItem(itemIndex++, cur, query.trim()));
       }
     }
     return $root;
@@ -64,10 +134,30 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
     $result.html(null);
     $resultItems = $('.search-result__item'); activeIndex = 0;
   }
-  function onInputNotEmpty(val) {
-    $result.html(render(searchByQuery(val)));
+
+  function renderQuery(val) {
+    $result.html(render(searchByQuery(val), val));
     $resultItems = $('.search-result__item'); activeIndex = 0;
     $resultItems.eq(0).addClass('active');
+  }
+
+  function onInputNotEmpty(val) {
+    if (searchDataLoaded) {
+      renderQuery(val);
+    } else if (!searchDataLoading) {
+      searchDataLoading = true;
+      window.Lazyload.js(PAHTS.search_js, function() {
+        searchData = window.TEXT_SEARCH_DATA || {};
+        searchDataLoaded = true;
+        searchDataLoading = false;
+        var currentVal = search.getVal && search.getVal();
+        if (currentVal) {
+          renderQuery(currentVal);
+        } else {
+          clear();
+        }
+      });
+    }
   }
 
   search.clear = clear;
